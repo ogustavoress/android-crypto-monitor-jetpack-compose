870147fd0e1405d8a3380980e7a6748f4ababf37
ogustavoress <ogustavoress@gmail.com>
2025-04-28 14:58:28 -0300

    Configurando a MainActivity.kt para executar com o layout pronto

--- app/src/main/java/ogustavoress/com/github/android_crypto_monitor/MainActivity.kt
+++ app/src/main/java/ogustavoress/com/github/android_crypto_monitor/MainActivity.kt
@@ -1,21 +1,25 @@
 package ogustavoress.com.github.android_crypto_monitor
 
 import android.os.Bundle
-import androidx.activity.ComponentActivity
-import androidx.activity.compose.setContent
-import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import ogustavoress.com.github.android_crypto_monitor.ui.theme.AndroidcryptomonitorTheme
 
-class MainActivity : ComponentActivity() {
+import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.Toolbar
+import ogustavoress.com.github.android_crypto_monitor.R.color.primary
+
+class MainActivity : AppCompatActivity(){
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
 
+        //Configurando a toolbar
+        val toolbarMain: Toolbar = findViewById(R.id.toolbar_main)
+        configureToolbar(toolbarMain)
+    }
+
+    private fun configureToolbar(toolbar: Toolbar){
+        setSupportActionBar(toolbar)
+        toolbar.setTitleTextColor(getColor(R.color.white))
+        supportActionBar?.setTitle(getColor(R.string.app_title))
+        supportActionBar?.setBackgroundDrawable(getDrawable(primary))
     }
 }
