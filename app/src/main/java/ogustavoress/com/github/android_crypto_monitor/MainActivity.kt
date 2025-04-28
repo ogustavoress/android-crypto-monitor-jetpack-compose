2225bf9408b20f4c508dd7a14debcfbe6de4fec6
ogustavoress <ogustavoress@gmail.com>
2025-04-28 11:23:12 -0300

    Limpando códigos não necessários da MainActivity.kt

--- app/src/main/java/ogustavoress/com/github/android_crypto_monitor/MainActivity.kt
+++ app/src/main/java/ogustavoress/com/github/android_crypto_monitor/MainActivity.kt
@@ -16,32 +16,6 @@ import ogustavoress.com.github.android_crypto_monitor.ui.theme.Androidcryptomoni
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContent {
-            AndroidcryptomonitorTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
-            }
-        }
-    }
-}
 
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    AndroidcryptomonitorTheme {
-        Greeting("Android")
     }
 }
